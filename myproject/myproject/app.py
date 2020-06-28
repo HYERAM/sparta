135f8a2274be0ba01f0fd9a72b3d947c885b9189
Hyeram Noh <nhr5568@gmail.com>
2020-06-28 16:02:43 +0900

    8th

--- myproject/myproject/app.py
+++ myproject/myproject/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb://test:test@localhost',27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
 
 
@@ -21,7 +21,7 @@ def home():
 @app.route('/tna/list', methods=['GET']) #스케쥴을 찾아 돌려준다.
 def tna_list():
     # 1. tna 전체 목록을 검색. id 제외하고 date가 빠른 순으로 정렬하기.
-    tna = list(db.justice_tna.find({},{'_id':False}).sort('due_date',1)) ## .sort() 부분 정리필요
+    tna = list(db.justice_tna.find({},{'_id':False}).sort('due_date',1)) 
     # 2. 성공하면 success 메시지와 함께 tna 목록을 클라이언트에 전달.
     return jsonify({'result': 'success','tna_list':tna})
 
